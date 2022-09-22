660b9eb25e06d55485061002ea743abc087b388b
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-09-22 19:20:29 -0400

    Update Script #3
    updated script

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -25,6 +25,11 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+
+        pos.x += speed * Time.deltaTime;
+
+        transform.position = pos;
+
     }
 }
