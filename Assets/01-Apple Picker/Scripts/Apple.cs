c9f40210a0b8f3eb234aa6384dc5bb3f6357c9e3
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-09-22 22:49:31 -0400

    Still In Work
    still in work

--- Assets/01-Apple Picker/Scripts/Apple.cs
+++ Assets/01-Apple Picker/Scripts/Apple.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+
     public static float bottomY = -20f;
 
     // Start is called before the first frame update
@@ -18,6 +20,10 @@ public class Apple : MonoBehaviour
        if (transform.position.y < bottomY)
        {
             Destroy(this.gameObject);
-       } 
+       }
+
+       ApplePicker apScript = Camera.main.ScreenToWorldPoint(apScript);
+
+       apScript.AppleDestroyed();
     }
 }
