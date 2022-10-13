7de2aa23c34bf43b95e73af5159833681b39d86c
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-10-12 22:37:31 -0400

    Huh

--- Assets/03-Prototype1/Scripts/Fruit.cs
+++ Assets/03-Prototype1/Scripts/Fruit.cs
@@ -21,15 +21,11 @@ public class Fruit : MonoBehaviour
         this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
     }
 
-
-
-
-
-
-
-
-
-
-
-
+    private  void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+    }
 }
