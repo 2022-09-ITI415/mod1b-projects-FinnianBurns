4afde050e9fbbda1db483ea7503bcca63d050ad2
FinnianBurns <fwb13@scarletmail.rutgers.edu>
2022-10-12 22:31:09 -0400

    Random Apples

--- Assets/03-Prototype1/Scripts/Fruit.cs
+++ Assets/03-Prototype1/Scripts/Fruit.cs
@@ -4,7 +4,31 @@ using UnityEngine;
 
 public class Fruit : MonoBehaviour
 {
+    public BoxCollider2D gridArea;
     
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+
+
+
+
+
+
+
+
 
 
 
